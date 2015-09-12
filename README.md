5445d645989fe79eb486fb69f257af729a723aab
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2015-09-12 21:27:18 +0900

    Initial commit.

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+File::Restore - It's new $module
+
+# SYNOPSIS
+
+    use File::Restore;
+
+# DESCRIPTION
+
+File::Restore is ...
+
+# LICENSE
+
+Copyright (C) Masahiro Iuchi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Masahiro Iuchi &lt;masahiro.iuchi@gmail.com>
---
+++ lib/File/Restore.pm
@@ -0,0 +1,39 @@
+package File::Restore;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+File::Restore - It's new $module
+
+=head1 SYNOPSIS
+
+    use File::Restore;
+
+=head1 DESCRIPTION
+
+File::Restore is ...
+
+=head1 LICENSE
+
+Copyright (C) Masahiro Iuchi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Masahiro Iuchi E<lt>masahiro.iuchi@gmail.comE<gt>
+
+=cut
+
